8df017c3e60126f9a16d4435e5146e7eaf52b46b
pink_glb <adriancosta1215@gmail.com>
2022-08-06 13:18:12 -0400

    ranking controller e router - rota get ranking com postgres

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import loginRouter from './src/routes/loginRouter.js'
 import urlsRouter from './src/routes/urlsRouter.js'
 import userRouter from './src/routes/userRouter.js'
+import rankingRouter from './src/routes/rankingRouter.js'
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ server.use(express.json());
 server.use(loginRouter);
 server.use(urlsRouter);
 server.use(userRouter);
+server.use(rankingRouter);
 
 server.listen(process.env.PORT, ()=>{
     console.log("Server running on port " + process.env.PORT)
---
+++ src/controllers/rankingController.js
@@ -0,0 +1,7 @@
+import connection from "../dbStrategy/postgres.js";
+
+export default async function getRanking(req, res){
+    const ranking =  await connection.query('SELECT users.id AS id, name, COUNT("userId") AS "linkCount", COALESCE(SUM("visitCount"), 0) AS "visitCount" FROM urls RIGHT JOIN users ON "userId" = users.id GROUP BY users.id, "visitCount" ORDER BY "visitCount" DESC LIMIT 10');
+    
+    res.send(ranking.rows).status(200);
+}
