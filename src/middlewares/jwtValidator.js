232c3a80634cd0851a9259a375806226a9bbc86a
pink_glb <adriancosta1215@gmail.com>
2022-08-07 22:04:44 -0400

    arrumando chave jwt validator

--- src/middlewares/jwtValidator.js
+++ src/middlewares/jwtValidator.js
@@ -4,7 +4,7 @@ export default function jwtValidation (req, res, next){
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(" ")[1];
     
-    jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded)=>{
+    jwt.verify(token, `${process.env.JWT_ACCESS_SECRET}`, (err, decoded)=>{
         if(err || token === null) return res.sendStatus(401);
         req.id = decoded.id;
         next();
