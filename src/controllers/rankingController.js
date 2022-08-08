a8f58d27d96eecc419ce97eea5dc7f672ad80e38
pink_glb <adriancosta1215@gmail.com>
2022-08-08 09:46:58 -0400

    adicionando try/catch aos controllers

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,7 +1,10 @@
 import connection from "../dbStrategy/postgres.js";
 
 export default async function getRanking(req, res){
-    const ranking =  await connection.query('SELECT users.id AS id, name, COUNT("userId") AS "linkCount", COALESCE(SUM("visitCount"), 0) AS "visitCount" FROM urls RIGHT JOIN users ON "userId" = users.id GROUP BY users.id, "visitCount" ORDER BY "visitCount" DESC LIMIT 10');
-    
-    res.send(ranking.rows).status(200);
+    try{
+        const ranking =  await connection.query('SELECT users.id AS id, name, COUNT("userId") AS "linkCount", COALESCE(SUM("visitCount"), 0) AS "visitCount" FROM urls RIGHT JOIN users ON "userId" = users.id GROUP BY users.id, "visitCount" ORDER BY "visitCount" DESC LIMIT 10');
+        res.send(ranking.rows).status(200);
+    }catch{
+        res.sendStatus(500);
+    }
 }
